88292e1e3cf7b5c214c1aebec22bfcf2c5ddaa30
Joshua <257735484+J-Moessmer@users.noreply.github.com>
2026-04-24 08:56:43 +0200

    Add files via upload

---
+++ Python Projects at the Heinrich-Heine University 2022/Project03/jupy.cmd
@@ -0,0 +1,1 @@
+"%userprofile%\Anaconda3\python.exe" "%userprofile%\Anaconda3\cwp.py" "%userprofile%\Anaconda3" "%userprofile%\Anaconda3\python.exe" "%userprofile%\Anaconda3\Scripts\jupyter-notebook-script.py" "%cd%"
